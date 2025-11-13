ca96013ebfa48f591be8dfeb5da08a9bc97f60b8
Noel Miller <nomiller@redhat.com>
2025-11-13 17:27:41 -0600

    fix path

--- cluster-system/ingress-nginx/install.sh
+++ cluster-system/ingress-nginx/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo ">>>>> Installing Ingress-nginx"
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx/ --force-update
-helm upgrade ingress-nginx ingress-nginx/ingress-nginx -n cluster-system -f ingress-nginx-values.yaml --wait --wait-for-jobs -i
+helm upgrade ingress-nginx ingress-nginx/ingress-nginx -n cluster-system -f ingress-nginx/ingress-nginx-values.yaml --wait --wait-for-jobs -i
 echo "<<<<< Ingress-nginx ready."
