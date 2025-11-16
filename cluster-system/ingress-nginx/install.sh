4a08cac83b3596597d60d787fd0272dd7a2006d5
Noel Miller <nomiller@redhat.com>
2025-11-16 07:24:18 -0600

    deleted ingress-nginx because it is being deprecated

--- cluster-system/ingress-nginx/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo ">>>>> Installing Ingress-nginx"
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx/ --force-update
-helm upgrade ingress-nginx ingress-nginx/ingress-nginx -n cluster-system -f ingress-nginx/ingress-nginx-values.yaml --wait --wait-for-jobs -i
-echo "<<<<< Ingress-nginx ready."
