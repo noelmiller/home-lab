817b47e86c156a9c2132922ccde0b8107a24fd64
Noel Miller <nomiller@redhat.com>
2025-11-24 00:59:42 -0600

    delete testing issuer, it's not needed

--- cluster-system/cert-manager/install.sh
+++ cluster-system/cert-manager/install.sh
@@ -64,5 +64,4 @@ kubectl apply -f cert-manager/secret.yaml
 
 helm upgrade cert-manager jetstack/cert-manager -n cluster-system -f cert-manager/cert-manager-values.yaml --wait --wait-for-jobs -i
 
-kubectl apply -f cert-manager/testing-issuer.yaml -n cluster-system
 kubectl apply -f cert-manager/production-issuer.yaml -n cluster-system
